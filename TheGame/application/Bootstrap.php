27af5547ffc0f4c15e741cc008080f3a0070d97f
peter.pekarovic <pekarovic.peter@gmail.com>
2014-08-10 19:11:43 +0200

    Aeron - acl

--- TheGame/application/Bootstrap.php
+++ TheGame/application/Bootstrap.php
@@ -2,9 +2,10 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
     
-    protected function _initSession() {
+    /*protected function _initSession() {
         Zend_Session::start();
-    }
+        Zend_Session::rememberMe(10); 
+    }*/
     
     protected function _initGameLibrary() {
         $loader = Zend_Loader_Autoloader::getInstance();
@@ -13,11 +14,26 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
     
     protected function _initLoginConfig() {
         $config = new Zend_Config_Ini(
-            APPLICATION_PATH . '/configs/login.ini', APPLICATION_ENV
+            APPLICATION_PATH . '/configs/login/login.ini', APPLICATION_ENV
         );
         
         Zend_Registry::set('login', $config);
         return $config; 
+    }
+    
+    protected function _initAcl() {         
+       if (Zend_Auth::getInstance()->hasIdentity()){
+            Zend_Registry::set ('role',
+                     Zend_Auth::getInstance()->getStorage()
+                                              ->read()
+                                              ->role);
+        }else{
+            Zend_Registry::set('role', 'guest');
+        }
+
+        $acl = new Application_Model_Acl();
+        Zend_Registry::set('acl', $acl);
+
     }
 
 }
--- TheGame/library/Game/Controller/Action.php
+++ TheGame/library/Game/Controller/Action.php
@@ -3,13 +3,27 @@
 
 class Game_Controller_Action extends Zend_Controller_Action {
     
+    private $_acl = null;
+    
     public function _construct() {
         parent::__construct();
+        
+    }
+    
+    public function preDispatch() {
+        //get request information
+        $resource = $this->getRequest()->getControllerName ();
+        $action = $this->getRequest()->getActionName ();
+
+        //check permissions
+        $acl = Zend_Registry::get('acl');
+        if(!$acl->isAllowed(Zend_Registry::get('role'), $resource, $action)){
+            $this->_forward("login", "user");
+        }
+        
     }
     
     public function init() {
-        if (!Zend_Auth::getInstance()->hasIdentity()) {
-            $this->_helper->redirector('login', 'user');
-        }
+
     }
 }
