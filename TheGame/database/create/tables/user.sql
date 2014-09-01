1f836e40af5ddecbd90581798385a36aa9ac4143
peter.pekarovic <pekarovic.peter@gmail.com>
2014-09-01 23:14:49 +0200

    Aeron - user entity db update

--- TheGame/database/create/tables/user.sql
+++ TheGame/database/create/tables/user.sql
@@ -3,7 +3,11 @@ CREATE TABLE IF NOT EXISTS `user` (
   `nickname` varchar(150) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` text NOT NULL,
-  `password_salt` char(16) NOT NULL,
+  `password_salt` varchar(38) NOT NULL,
+  `role` varchar(20) NOT NULL DEFAULT 'basic_user',
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `language` varchar(4) NOT NULL DEFAULT 'en',
   PRIMARY KEY (`id_user`),
-  UNIQUE KEY `nickname` (`nickname`,`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  UNIQUE KEY `nickname` (`nickname`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
